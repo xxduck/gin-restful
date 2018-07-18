8418bfda7db7404ddfb99715643dc4373cab741d
xiaofang-git <fang.1995@outlook.com>
2018-07-18 10:29:18 +0800

    定义首页信息，返回api地图

--- src/runserver.go
+++ src/runserver.go
@@ -4,7 +4,6 @@ import (
 	"gin-restful/src/databases"
 	"gin-restful/src/models"
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"gin-restful/src/middlewares"
 )
 
@@ -33,9 +32,7 @@ func main() {
 
 	// 定义主页请求方法
 	app.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"status": "ok",
-		})
+		c.JSON(200, app.Routes())
 	})
 
 	app.Run()
